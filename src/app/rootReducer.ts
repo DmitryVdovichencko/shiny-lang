558ad2dd691864a3d6234662e6ed803b8f0d64ef
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-05-12 10:30:16 +0500

    start working with routing

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
-export const history = createBrowserHistory();
 
 const rootReducer = combineReducers({
-	// router: connectRouter(history),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
