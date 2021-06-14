b61659d952bc1c12578c97a62fd99c634e97bbad
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-06-14 10:02:13 +0500

    add redux-saga support in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./rootReducer";
 
-export const store = configureStore({
-  reducer: rootReducer,
-})
-
+export const configureAppStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: [...getDefaultMiddleware({ thunk: false }), ...middlewares],
+  });
+  return store;
+};
+const store = configureAppStore();
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof rootReducer>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+
+export { store }
